0ab4316864f0c1b541cbd5cf70e6b6426b956ff9
firdausaurus <79572984+firdausaurus@users.noreply.github.com>
2021-02-24 17:05:36 +0700

    Create tentang.php

---
+++ tentang.php
@@ -0,0 +1,30 @@
+<?php  
+include "header.php";  
+?>  
+
+<div class="container padding=">  
+    <div class="row welcome text-center">  
+      <div class="col-12">  
+        <h1 >SAMPINGIN</h1>
+        <h2> Alasan website ini berdiri </h2>
+        <hr>  
+      </div>  
+      <div class="col-12">  
+        <p class="lead">Website ini dibuat karena PEMBUAT merasa prihatin kepada anak-anak yang baru saja lulus dari bangku sekolah SMA/SMK, tetapi sangat sulit untuk mencari pekerjaan. Ditengah ke prihatinan tersebut PEMBUAT sangat ingin sekali membantu mereka untuk mendapatkan pekerjaan . Mungkin dengan dibuat nya website SAMPINGIN ini setidaknya bisa membantu mereka untuk mencari pekerjaan dan bisa mengurangi angka pengangguran di Negara INDONESIA tercinta ini.  </p>  
+      </div>  
+    </div>  
+    </div>  
+      
+    <!--- Three Column Section -->  
+    <div class="container padding">  
+    <div class="row text-center padding">  
+      <div class="col-xs-12 col-sm-6 col-md">  
+        <img src="admin/assets/img/banner.png " width="200" height="200">  
+        <h3>FOUNDER</h3>  
+        <p>MUHAMMAD RIFQI FIRDAUS</p>  
+      </div>  
+        
+    </div>  
+    </div>  
+      
+
